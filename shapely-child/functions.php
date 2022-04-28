4272726600d647fb7a7bac87b70a08c68a9930f5
Doriane <a.adnot@gmail.com>
2022-04-28 11:18:38 +0200

    fix functions.php

--- shapely-child/functions.php
+++ shapely-child/functions.php
@@ -1,10 +1,5 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
-        array( 'parenthandle' ), 
-        wp_get_theme()->get('Version') // this only works if you have Version in the style header
-    );
+add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
+function wpm_enqueue_styles(){
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-
-?>
